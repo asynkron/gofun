9743c675e30b2e89af33aeb07c18b71d221a8c42
Roger Johansson <roger.johansson@charge-amps.com>
2022-03-28 14:36:27 +0200

    .

--- enumerable/func-enumerable.go
+++ enumerable/func-enumerable.go
@@ -7,29 +7,3 @@ type FuncEnumerable[T any] struct {
 func (e *FuncEnumerable[T]) Enumerate(yield Yield[T]) {
 	e.enumerateFun(yield)
 }
-
-//
-//func (e *FuncEnumerable[T]) Filter(predicate func(T) bool) Enumerable[T] {
-//	return Filter[T](e, predicate)
-//}
-//
-//func (e *FuncEnumerable[T]) FirstOrDefault(defaultValue T) T {
-//	return FirstOrDefault[T](e, defaultValue)
-//}
-//
-//func (e *FuncEnumerable[T]) LastOrDefault(defaultValue T) T {
-//	return LastOrDefault[T](e, defaultValue)
-//}
-//
-//func (e *FuncEnumerable[T]) ToSlice() []T {
-//	return ToSlice[T](e)
-//}
-//
-//func (e *FuncEnumerable[T]) Count() int {
-//	return Count[T](e)
-//}
-//
-//func (e *FuncEnumerable[T]) ElementAtOrDefault(index int, defaultValue T) T {
-//	return ElementAtOrDefault[T](e, index, defaultValue)
-//}
-//
