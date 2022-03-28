fe9868e721a655f960b35651384b331e7071cbe7
Roger Johansson <roger.johansson@charge-amps.com>
2022-03-28 14:39:03 +0200

    .

--- enumerable/enumerable.go
+++ enumerable/enumerable.go
@@ -9,12 +9,6 @@ type Yield[T any] func(item T) bool
 
 type Enumerable[T any] interface {
 	Enumerate(yield Yield[T])
-	//Filter(predicate func(T) bool) Enumerable[T]
-	//FirstOrDefault(defaultValue T) T
-	//LastOrDefault(defaultValue T) T
-	//ToSlice() []T
-	//Count() int
-	//ElementAtOrDefault(index int, defaultValue T) T
 
 	//Cannot make Map/Select as methods cannot have type parameters
 }
